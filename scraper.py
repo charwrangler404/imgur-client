59e443d3784a37139e617fbdad4d0ebf709df360
Elia Farin <efarin@inoc.com>
2020-02-07 16:24:02 -0600

    added selenium interface to search for albums

--- scraper.py
+++ scraper.py
@@ -4,12 +4,13 @@ from imgurpython import ImgurClient
 from pprint import pprint
 from helpers import get_input, get_config 
 import requests, argparse, json
+from selenium import webdriver
+
 
 # Set up command-line arguments
 parser = argparse.ArgumentParser('scraper.py')
 parser.add_argument('query', help='Search query')
-parser.add_argument('max_galleries', defualt='0', help='Maximum albums to scrape')
-parser.add_argument('-v', '--verbose', default=False, help='Turn on verbose output')
+parser.add_argument('destination', help='Download destination')
 args = parser.parse_args()
 
 # Authenticate function
@@ -23,19 +24,25 @@ def authenticate():
 
     return client
 
-def search(query, max_galleries)
+def search(query)
    #TODO: Search for a number of galleries that match the query
    #TODO: accumulate the galleries found into a list 'galleries'
+   galleries = []
+    browser = webdriver.Firefox()
+    browser.get(query)
+    for gallery in browser.find_elements_by_class_name('image-list-link')
+        link = 'https:' + gallery.get_attribute('href')
+        galleries.append(link)
+    browser.quit()
+    return galleries
 
-   return galleries
-
-def scrape_album(gallery):
-    # TODO - download images in the gallery
+def scrape_album(gallery, destination):
+    # TODO - download images in the gallery to the destination folder
 
 if __name__ == '__main__':
     client = authenticate()
 
-    galleries = search(args.query, args.max_galleries)
+    galleries = search(args.query)
 
     for gallery in galleries:
-        scrape_album(gallery)
+        scrape_album(gallery, args.destination)
